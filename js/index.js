ed39ef015dbc292a7b0559f5f0662481647b21b4
MiaLuna23 <166016904+MiaLuna23@users.noreply.github.com>
2024-04-25 08:21:19 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let usuarioPersona= "JuanGarcia23"
+let contraseñaUsuario= "cJzoht2!"
+
+if (usuarioPersona == "JuanGarcia23") && (contraseñaUsuario =="cJzoht2!") {
+    alert = "Bienvenido Juan!!!"
+} else {
+    alert ="vuelve a intentar"
+}
+
+//Punto 2
+let notaExamen=8
+let notaTrabajos=6
+let asistenciaAlumno= true
+if (notaExamen>8) {
+    
+} else {
+    
+}
